bcbe85d41b96de58c5e3b702e5ff514137e506ed
Baki78 <bakiget78@gmail.com>
2024-08-02 16:42:07 +0300

    setup done

--- server/index.mjs
+++ server/index.mjs
@@ -1,8 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+dotenv.config()
 const app = express()
 
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+app.get("/shorturls", (req, res) => {
+    
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
 })
